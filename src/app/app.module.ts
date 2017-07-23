e10e45c169bdf2b07d16647587966c375c23a0ae
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-07-23 23:12:49 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'employee', component: EmployeeComponent },
+      { path: 'product', component: ProductComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
